ee92d10f27805817af62523a3e15a525ae304b90
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-26 10:33:35 -0300

    sas

--- src/components/DetallePlato.jsx
+++ src/components/DetallePlato.jsx
@@ -1,54 +1,62 @@
-import React,{useEffect,useState} from "react";
-import { View, Text, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { useDataContext } from "../Context";
 import { Card } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
-import {Button} from "react-native-paper";
+import { Button } from "react-native-paper";
 
-
-const DetallePlato = ( props) => {
+const DetallePlato = (props) => {
   const [data, setData] = useState(null);
 
-  const Idplato = props.route.params
+  const Idplato = props.route.params;
   console.log(Idplato);
   useEffect(() => {
     const fetchDataFromAPI = async () => {
       try {
-        const response = await fetch(`https://api.spoonacular.com/recipes/${Idplato.Idplato}/information?apiKey=d2b669cce7d641cd8c8ab976e4125928`);
-          const data = await response.json();
-           setData(data);
+        const response = await fetch(
+          `https://api.spoonacular.com/recipes/${Idplato.Idplato}/information?apiKey=2f3ecdfbb20e494da7885893868a2b7e`
+        );
+        const data = await response.json();
+        setData(data);
       } catch (error) {
         console.error("Error al realizar la solicitud:", error);
       }
     };
-    
+
     fetchDataFromAPI();
   }, []);
-    const navigation = useNavigation();
-    console.log(data)
-    console.log(Idplato)
-  if (!data) { 
+  const navigation = useNavigation();
+  console.log(data);
+  if (!data) {
     return <Text>No hay datos disponibles.</Text>;
   }
 
-  
+  console.log(data.vegetarian);
+  const styles = StyleSheet.create({
+    foto: {
+      width: 100,
+      height: 100,
+    },
+  });
+
   return (
-    <View>
-      
-        <Card  style={{ margin: 10 }}>
-        <Card.Cover  />
-        <Card.Content>
-          <Text>{if(data.vegetarian==true)
-          {
-            
-          }}</Text>
-        </Card.Content>
-        
-      </Card>
-      </View>
+    <>
+    {data &&(<View>
+      <h1>{data.title}</h1>
+      <Image
+        source={{
+          uri: data.Image,
+        }}
+        style={styles.foto}
+      />
+      <h2>Porciones: {data.servings}</h2>
+        <h2>Vegano: {data.vegan}</h2>
+        <h2>Saludable:{data.veryHealthy}</h2>
+        <h2>Precio: ${data.pricePerServing}</h2>
+        <h2>Listo en: {data.readyInMinutes}</h2>
+    </View>)}
+    </>
   );
 };
 
 export default DetallePlato;
-
-
